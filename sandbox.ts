8728e351812d99befd97a8e4f413ae5072394107
DammyD <ibukunoluwadawodu@gmail.com>
2023-09-27 21:53:15 +0100

    Typescript lesson one and two

---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const character = 'luigi';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+});
+
+// Values can be changed but types cannot
