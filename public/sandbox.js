69f2a7adff8334789d7b54b6b9bbfa0d980c538f
DammyD <ibukunoluwadawodu@gmail.com>
2023-09-30 17:28:33 +0100

    lesson eight

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('mario');
+// Functions
+// let greet = () => {
+//     console.log('hello, again');
+// }
+// greet = 'hello'
+let greet;
+greet = () => {
+    console.log('Hello, again');
+};
+// With the ? the value becomes optional but when a value is passed the ? is not needed
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,24 @@
-console.log('mario');
+// Functions
+// let greet = () => {
+//     console.log('hello, again');
+// }
+
+// greet = 'hello'
+
+let greet: Function;
+
+greet = () => {
+    console.log('Hello, again');
+}
+// With the ? the value becomes optional but when a value is passed the ? is not needed
+const add = (a: number, b: number, c: number | string = 10) => {
+console.log(a + b);
+console.log(c);
+}
+add(5, 10, '20');
+
+const minus = (a: number, b: number) => {
+return a + b;
+}
+
+let result = minus(10, 7);
